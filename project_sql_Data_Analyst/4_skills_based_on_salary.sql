df8a66b96b7d8b02952d74e18ff72f2c7cd9e089
badeaandrei99 <b.andrei910@yahoo.com>
2025-12-07 19:41:05 +0200

    upload sql files

---
+++ project_sql_Data_Analyst/4_skills_based_on_salary.sql
@@ -0,0 +1,34 @@
+/* 1. What are the top paying jobs for my role?
+2. What are the skills required for these top paying roles?
+3. What are the most in demand skills for my role?
+4. What are the top skills based on salary for my role?
+5. What are the most optimal skills to learn?
+ a. Optimal: High demand and high paying */
+
+
+--4.What are the top skills based on salary for my role?--
+
+WITH job_skills AS (SELECT 
+    job_title_short,
+     job_title,
+     salary_year_avg,
+     job_location,
+     name AS company_name,
+     skills,
+     type AS skill_type
+FROM job_postings_fact
+LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
+INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+Where job_title_short = 'Data Analyst' AND
+    job_location = 'Anywhere'
+)
+
+SELECT
+    ROUND(AVG(salary_year_avg),0)AS year_salary,
+    skills
+FROM job_skills
+WHERE salary_year_avg IS NOT NULL
+GROUP BY skills
+ORDER BY year_salary DESC;
+
