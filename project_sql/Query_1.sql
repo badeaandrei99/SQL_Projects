b73b204ee2642ee03f803a22ed3e2e729e1ce9bb
badeaandrei99 <b.andrei910@yahoo.com>
2025-12-07 11:35:15 +0200

    query_2.sql

--- project_sql/Query_1.sql
+++ project_sql/Query_1.sql
@@ -1,2 +1,25 @@
-select* from company_dim
-limit 50;
+/* 1. What are the top paying jobs for my role?
+2. What are the skills required for these top paying roles?
+3. What are the most in demand skills for my role?
+4. What are the top skills based on salary for my role?
+5. What are the most optimal skills to learn?
+ a. Optimal: High demand and high paying */
+
+
+--1. What are the top paying jobs for my role?--
+
+SELECT 
+    job_title_short,
+     job_title,
+     salary_year_avg,
+     job_location,
+     name AS company_name
+FROM job_postings_fact
+LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
+Where job_title_short = 'Data Analyst' AND
+    salary_year_avg IS NOT NULL AND
+    job_location = 'Anywhere'
+ORDER BY salary_year_avg DESC
+LIMIT 10;
+
+
---
+++ project_sql/Query_2.sql
@@ -0,0 +1,19 @@
+--2. What are the skills required for these top paying roles?--
+
+SELECT 
+    job_title_short,
+     job_title,
+     salary_year_avg,
+     job_location,
+     name AS company_name,
+     skills,
+     type AS skill_type
+FROM job_postings_fact
+LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
+INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+Where job_title_short = 'Data Analyst' AND
+    salary_year_avg IS NOT NULL AND
+    job_location = 'Anywhere'
+ORDER BY salary_year_avg DESC
+LIMIT 20;
